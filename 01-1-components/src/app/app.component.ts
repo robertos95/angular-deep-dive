78c03b27721aa712ef0e732a95069749dc7ea0a8
Roberto Salim <roberto.salim@wartsila.com>
2025-06-30 13:44:37 +0800

    feat: @Output - Custom Events and Event Emitters

--- 01-1-components/src/app/app.component.ts
+++ 01-1-components/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
 
 @Component({
     selector: 'app-root',
@@ -9,4 +10,8 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
     courses = COURSES;
+
+    onCourseSelected(course: Course) {
+        console.log('Course selected: ', course);
+    }
 }
--- 01-1-components/src/app/course-card/course-card.component.ts
+++ 01-1-components/src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
@@ -12,4 +12,13 @@ export class CourseCardComponent {
     required: true,
   })
   course: Course;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    console.log('Card component - button clicked ...');
+    
+    this.courseSelected.emit(this.course);
+  }
 }
