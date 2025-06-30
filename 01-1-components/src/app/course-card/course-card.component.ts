e9484c2a4ac57f755abd709a89fc5d98cc3bc551
Roberto Salim <roberto.salim@wartsila.com>
2025-06-30 11:58:04 +0800

    feat: create 1st component

---
+++ 01-1-components/src/app/course-card/course-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css',
+})
+export class CourseCardComponent {
+
+}
