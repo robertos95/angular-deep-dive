04fc4eb4f818fb7117ec847487dd1a82063e3df7
Roberto Salim <roberto.salim@wartsila.com>
2025-07-02 16:37:52 +0800

    feat: creating custom service (coursesService)

--- 03-3-services/src/app/app.component.ts
+++ 03-3-services/src/app/app.component.ts
@@ -13,6 +13,7 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
 import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
   selector: "app-root",
@@ -23,9 +24,13 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private coursesService: CoursesService
+  ) {}
 
   ngOnInit() {
+    console.log(this.coursesService);
     const params = new HttpParams().set("page", "1").set("pageSize", "10");
     this.courses$ = this.http.get<Course[]>("/api/courses", { params });
   }
