f608c83cc5bd03baf6fd8ae65ff93acc2ffb07b2
Roberto Salim <roberto.salim@wartsila.com>
2025-07-02 15:15:23 +0800

    feat: angular custom structural directives ngxUnless

--- 02-2-directives/src/app/app.module.ts
+++ 02-2-directives/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { CourseImageComponent } from "./course-image/course-image.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
+import { NgxUnlessDirective } from "./directives/ngx-unless.directive";
 
 @NgModule({
   declarations: [AppComponent, CourseCardComponent, CourseImageComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HighlightedDirective],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HighlightedDirective,
+    NgxUnlessDirective,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
