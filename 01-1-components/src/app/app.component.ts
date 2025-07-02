8a2cf772734d81f286d143453aaa932591ae8703
Roberto Salim <roberto.salim@wartsila.com>
2025-07-02 13:02:13 +0800

    feat: add course-image and use if else to show it

--- 01-1-components/src/app/app.component.ts
+++ 01-1-components/src/app/app.component.ts
@@ -1,28 +1,27 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import {COURSES} from '../db-data';
-import { Course } from './model/course';
-import { CourseCardComponent } from './course-card/course-card.component';
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
 export class AppComponent implements AfterViewInit {
-    courses = [...COURSES];
+  courses = [...COURSES];
 
+  @ViewChild(CourseCardComponent)
+  card: CourseCardComponent;
 
-    @ViewChild(CourseCardComponent)
-    card: CourseCardComponent;
+  onCourseSelected(course: Course) {
+    console.log("Course selected: ", this.card);
+  }
 
-    onCourseSelected(course: Course) {
-        console.log('Course selected: ', this.card);
-    }
-
-    ngAfterViewInit() {
-        console.log('Card component: ', this.card);
-        console.log('Card component course: ', this.card.course);
-        console.log('Card component index: ', this.card.index);
-    }
+  ngAfterViewInit() {
+    console.log("Card component: ", this.card);
+    console.log("Card component course: ", this.card.course);
+    console.log("Card component index: ", this.card.index);
+  }
 }
--- 01-1-components/src/app/course-card/course-card.component.ts
+++ 01-1-components/src/app/course-card/course-card.component.ts
@@ -1,11 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Course } from '../model/course';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Course } from "../model/course";
 
 @Component({
-  selector: 'course-card',
-  imports: [],
-  templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css',
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
 })
 export class CourseCardComponent {
   @Input({
@@ -22,8 +21,8 @@ export class CourseCardComponent {
   courseSelected = new EventEmitter<Course>();
 
   onCourseViewed() {
-    console.log('Card component - button clicked ...');
-    
+    console.log("Card component - button clicked ...");
+
     this.courseSelected.emit(this.course);
   }
 }
