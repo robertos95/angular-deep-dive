50ef66fa3635752f7c532335a65578120fb343a2
Roberto Salim <roberto.salim@wartsila.com>
2025-07-02 13:42:18 +0800

    feat: add first attribute directive

--- 02-2-directives/src/app/app.module.ts
+++ 02-2-directives/src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { CourseImageComponent } from './course-image/course-image.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { CourseImageComponent } from "./course-image/course-image.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseCardComponent,
-    CourseImageComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, CourseCardComponent, CourseImageComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, HighlightedDirective],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
